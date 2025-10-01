f0e66457b8bcc25837c1b3e19d81e1a512855412
wookyoung0727@gmail.com <wookyoung0727@gmail.com>
2025-10-01 16:46:31 +0900

    change_eval_default

--- experiments/dct_tokenizer/eval_qwen_default.sh
+++ experiments/dct_tokenizer/eval_qwen_default.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Configuration variables
-NUM_PROC_PER_NODE=4  # Set the number of processes per node
+NUM_PROC_PER_NODE=1  # Set the number of processes per node
 # CP_SIZE=2
 TP_SIZE=1 ### 
 PP_SIZE=1 ### PP_4 
-IMAGE_PATH='PATH_TO_YOUR_IMAGE'
+# IMAGE_PATH='PATH_TO_YOUR_IMAGE'
 # Exp logging path
 # Construct the arguments stringring
 ARGS=(
   "--load_from_hf"
-  "--image_url" "$IMAGE_PATH"
+  # "--image_url" "$IMAGE_PATH"
   "--tp_size" "$TP_SIZE"
-  "--pp_size"ㅠ "$PP_SIZE"
+  "--pp_size" "$PP_SIZE"
   )
 
 # Run the experiment with torchrun
