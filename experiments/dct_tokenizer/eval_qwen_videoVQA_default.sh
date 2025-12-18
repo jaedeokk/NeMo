523432fc79ed0a752fb66199e3d73e911976d088
WooKyoungHan <wookyoung0727@gmail.com>
2025-12-18 20:54:46 +0900

    Folder eval added

--- experiments/dct_tokenizer/eval_qwen_videoVQA_default.sh
+++ experiments/dct_tokenizer/eval_qwen_videoVQA_default.sh
@@ -5,18 +5,25 @@ NUM_PROC_PER_NODE=1  # Set the number of processes per node
 # CP_SIZE=2
 TP_SIZE=1 ### 
 PP_SIZE=1 ### PP_4 
-# IMAGE_PATH='/datasets/TextVQA/val/train_images/5ce862cbefd8458f.jpg'
-IMAGE_PATH='/datasets/msrvtt/videos/video7011.mp4'
 
+IMAGE_PATH='/datasets/msrvtt/videos/'
+JSON_PATH='/datasets/msrvtt/QA/test_qa.json'
+
+MODEL_SIZE="2B"
+OUTPUT_PATH="/datasets/msrvtt/results/pred_videoVQA_val_${MODEL_SIZE}.json"
+OSL=10
 # Exp logging path
 # Construct the arguments stringring
 ARGS=(
   "--load_from_hf"
-  "--image_url" "$IMAGE_PATH"
   "--tp_size" "$TP_SIZE"
   "--pp_size" "$PP_SIZE"
-  "--osl" 30
-    )
+  "--video_root" "$IMAGE_PATH"
+  "--json_path" "$JSON_PATH"
+  "--output_json" "$OUTPUT_PATH"
+  "--model_size" "$MODEL_SIZE"
+  "--osl" $OSL
+  )
 
 # Run the experiment with torchrun
 
@@ -24,4 +31,4 @@ echo "NUM_PROC_PER_NODE: ${NUM_PROC_PER_NODE}"
 echo "ARGS: ${ARGS[@]}"
 
 torchrun --nproc_per_node=$NUM_PROC_PER_NODE \
-    /workspace/scripts/vlm/qwen2vl_generate_video.py ${ARGS[@]}
+    /workspace/scripts/vlm/qwen2vl_videoVQA_eval.py ${ARGS[@]}
